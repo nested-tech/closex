014a7df5cdc13128b046c80dabd7993deb700f8c
Sam Davies <sampdavies@gmail.com>
2017-10-06 15:08:38 +0100

    Add metadata for Hex

--- mix.exs
+++ mix.exs
@@ -11,16 +11,15 @@ defmodule Closex.Mixfile do
       lockfile: "mix.lock",
       elixirc_paths: elixirc_paths(Mix.env),
       elixir: "~> 1.5",
+      description: description(),
+      package: package(),
+      deps: deps(),
       start_permanent: Mix.env == :prod,
-      deps: deps()
     ]
   end
 
   def application do
-    [
-      extra_applications: [:logger],
-      mod: {Closex.Application, []}
-    ]
+    []
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
@@ -31,6 +30,21 @@ defmodule Closex.Mixfile do
       {:poison, "~> 3.1.0"},
       {:httpoison, "~> 0.13.0"},
       {:exvcr, "~> 0.8", only: :test},
+    ]
+  end
+
+  defp description() do
+    "Close.io HTTP client for Elixir"
+  end
+
+  defp package() do
+    [
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["Sam Davies", "Darren Oakley"],
+      licenses: ["MIT"],
+      source_url: "https://github.com/nextdayproperty/closex",
+      homepage_url: "https://github.com/nextdayproperty/closex",
+      links: %{"Nested" => "https://nested.com", "Close.io" => "https://close.io", "GitHub" => "https://github.com/nextdayproperty/closex"}
     ]
   end
 end
