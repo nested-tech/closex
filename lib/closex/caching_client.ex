b63e2ff1dd032b78c0eec20517971e63145236d4
Sam Davies <sampdavies@gmail.com>
2017-10-13 16:02:06 +0100

    Cache wrap opportunity

--- lib/closex/caching_client.ex
+++ lib/closex/caching_client.ex
@@ -5,8 +5,34 @@ defmodule Closex.CachingClient do
   defdelegate find_leads(search_term, opts \\ []), to: @fallback_client
 
   def get_lead(lead_id, opts \\ []) do
-    Cachex.get(:closex_cache, lead_id, fallback: fn lead_id ->
-      case @fallback_client.get_lead(lead_id, opts) do
+    get_cached(lead_id, {@fallback_client, :get_lead, [lead_id, opts]})
+  end
+
+  defdelegate create_lead(payload, opts \\ []), to: @fallback_client
+  defdelegate update_lead(lead_id, payload, opts \\ []), to: @fallback_client
+
+  def get_opportunity(opportunity_id, opts \\ []) do
+    get_cached(opportunity_id, {@fallback_client, :get_opportunity, [opportunity_id, opts]})
+  end
+
+  defdelegate create_opportunity(payload, opts \\ []), to: @fallback_client
+  defdelegate update_opportunity(opportunity_id, payload, opts \\ []), to: @fallback_client
+
+  defdelegate get_lead_custom_field(custom_field_id, opts \\ []), to: @fallback_client
+
+  defdelegate get_organization(organization_id, opts \\ []), to: @fallback_client
+
+  defdelegate get_lead_statuses(opts \\ []), to: @fallback_client
+
+  defdelegate get_opportunity_statuses(opts \\ []), to: @fallback_client
+
+  defdelegate send_email(payload, opts \\ []), to: @fallback_client
+
+  defdelegate get_users(opts \\ []), to: @fallback_client
+
+  defp get_cached(key, {mod, fun, args}) do
+    Cachex.get(:closex_cache, key, fallback: fn key ->
+      case apply(mod, fun, args) do
         success = {:ok, _} ->
           {:commit, success}
         error = {:error, _} ->
@@ -14,16 +40,4 @@ defmodule Closex.CachingClient do
       end
     end)
   end
-
-  defdelegate create_lead(payload, opts \\ []), to: @fallback_client
-  defdelegate update_lead(lead_id, payload, opts \\ []), to: @fallback_client
-  defdelegate get_opportunity(opportunity_id, opts \\ []), to: @fallback_client
-  defdelegate create_opportunity(payload, opts \\ []), to: @fallback_client
-  defdelegate update_opportunity(opportunity_id, payload, opts \\ []), to: @fallback_client
-  defdelegate get_lead_custom_field(custom_field_id, opts \\ []), to: @fallback_client
-  defdelegate get_organization(organization_id, opts \\ []), to: @fallback_client
-  defdelegate get_lead_statuses(opts \\ []), to: @fallback_client
-  defdelegate get_opportunity_statuses(opts \\ []), to: @fallback_client
-  defdelegate send_email(payload, opts \\ []), to: @fallback_client
-  defdelegate get_users(opts \\ []), to: @fallback_client
 end
