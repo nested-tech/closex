f53e45a467c940ecf2e4fc9a6f1483bce724d28c
Sam Davies <sampdavies@gmail.com>
2017-10-13 09:30:58 +0100

    Extract client functions into behaviour

---
+++ lib/closex/client_behaviour.ex
@@ -0,0 +1,23 @@
+defmodule Closex.ClientBehaviour do
+  @moduledoc "Tools for interacting with the Close.io API"
+
+  @type id :: String.t
+  @type opts :: Keyword.t
+  @type success :: {:ok, map}
+  @type error :: {:error, any}
+  @type result :: success | error
+
+  @callback find_leads(String.t, opts) :: result
+  @callback get_lead(id, opts) :: result
+  @callback create_lead(map, opts) :: result
+  @callback update_lead(id, map, opts) :: result
+  @callback get_opportunity(id, opts) :: result
+  @callback create_opportunity(map, opts) :: result
+  @callback update_opportunity(id, map, opts) :: result
+  @callback get_lead_custom_field(id, opts) :: result
+  @callback get_organization(id, opts) :: result
+  @callback get_lead_statuses(opts) :: result
+  @callback get_opportunity_statuses(opts) :: result
+  @callback send_email(map, opts) :: result
+  @callback get_users(opts) :: result
+end
