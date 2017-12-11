537eca53c457631a1826f38047653d848a1e74a8
Sam Davies <sampdavies@gmail.com>
2017-12-11 14:04:35 +0000

    Bump version to 0.5.9
    - FEATURE: find_opportunities

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Closex.Mixfile do
   def project do
     [
       app: :closex,
-      version: "0.5.8",
+      version: "0.5.9",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
