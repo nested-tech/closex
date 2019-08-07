fadedea3c62db7a804e50319b16fb596d7d0bea0
Adz <email@test.example>
2019-08-07 12:28:34 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Closex.Mixfile do
   def project do
     [
       app: :closex,
-      version: "1.4.1",
+      version: "1.4.2",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
